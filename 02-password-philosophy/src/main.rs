3ba0b81f2b3771e21ccad3524aefb16504f969d0
Jimmy Chu <jimmychu0807@gmail.com>
2021-03-14 12:19:50 +0800

    Solving Day 2 Part 1

--- 02-password-philosophy/src/main.rs
+++ 02-password-philosophy/src/main.rs
@@ -1,23 +1,71 @@
+#![allow(dead_code)]
+
+use std::fs;
+use lazy_static::lazy_static;
 use regex::Regex;
 
-const test_data: [&str; 3] = [
+const TEST_DATA: [&str; 3] = [
 	"1-3 a: abcde",
 	"1-3 b: cdefg",
-	"2-9 c: cccccccc"
+	"2-10 c: cccccccc"
 ];
 
+const INPUT_PATH: &str = "02-password-philosophy/data/input.dat";
+
 fn main() {
-	let count = test_data
+	let count = read_input(INPUT_PATH).unwrap()
 		.iter()
-		.filter(|phrase| valid_password(phrase))
-		.collect::<Vec<&&str>>()
+		.filter(|phrase| valid_password(phrase).unwrap_or(false))
+		.collect::<Vec<_>>()
 		.len();
 
-	println!("count: {}", count);
+	println!("valid password: {}", count);
 }
 
-fn valid_password(phrase: &str) -> bool {
-	let regex: Regex = Regex::new(r"(\d+)-(\d+)\s+(.):\s+(\S)")
-		.unwrap();
+fn read_input(path: &str) -> Result<Vec<String>, &'static str> {
+	let contents = fs::read_to_string(path)
+		.map_err(|_| "File cannot be read")?;
 
+	let lines: Vec<String> = contents.split("\n")
+		.map(|line| line.to_string())
+		.collect();
+
+	println!("File line count: {}", lines.len());
+
+	Ok(lines)
+}
+
+fn valid_password(phrase: &str) -> Result<bool, &'static str> {
+	lazy_static! {
+		static ref RE: Regex = Regex::new(r"^(\d+)-(\d+)\s+(.):\s+(\S+)$")
+			.unwrap();
+	}
+
+	let captures = RE.captures(phrase).ok_or("unknown phrase for parsing")?;
+
+	let min: u64 = captures.get(1)
+		.ok_or("min value not found")?
+		.as_str().parse()
+		.map_err(|_| "unknown min value")?;
+
+	let max: u64 = captures.get(2)
+		.ok_or("max value not found")?
+		.as_str().parse()
+		.map_err(|_| "unknown max value")?;
+
+	let check_char: char = captures.get(3)
+		.ok_or("check char not found")?
+		.as_str().parse()
+		.map_err(|_| "unknown check char")?;
+
+	let passphrase: &str = captures.get(4)
+		.ok_or("pass phrase not found")?
+		.as_str();
+
+	let cnt: u64 = passphrase.chars()
+		.filter(|c| *c == check_char)
+		.collect::<Vec<_>>()
+		.len() as u64;
+
+	Ok(cnt >= min && cnt <= max)
 }
