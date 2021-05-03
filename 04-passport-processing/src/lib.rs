db6186318c764256fa34325d2274080221672f92
Jimmy Chu <jimmychu0807@gmail.com>
2021-05-03 12:49:41 +0800

    working on field validation

--- 04-passport-processing/src/lib.rs
+++ 04-passport-processing/src/lib.rs
@@ -7,8 +7,6 @@ use std::collections::HashMap;
 #[cfg(test)]
 mod tests;
 
-const PASSPORT_REQ_KEYS: [&str; 7] = ["byr", "eyr", "iyr", "ecl", "hcl", "hgt", "pid"];
-
 // -- ReadState enum --
 
 #[derive(Clone, PartialEq, Debug)]
@@ -50,11 +48,23 @@ pub enum PassportProcessError {
 #[derive(Clone, PartialEq, Debug)]
 pub enum PassportInvalid {
 	MissingField(String),
+	OutOfRange(String, u32),
+	InvalidFormat(String, String),
 }
 
 impl From<&str> for PassportInvalid {
 	fn from(val: &str) -> Self {
 		Self::MissingField(val.to_string())
+	}
+}
+
+impl PassportInvalid {
+	pub fn out_of_range(key: &str, val: u32) -> Self {
+		Self::OutOfRange(key.to_string(), val)
+	}
+
+	pub fn invalid_format(key: &str, val: &str) -> Self {
+		Self::InvalidFormat(key.to_string(), val.to_string())
 	}
 }
 
@@ -94,10 +104,65 @@ impl Passport {
 	}
 
 	pub fn valid(&self) -> Result<(),Vec<PassportInvalid>> {
+		const PASSPORT_REQ_KEYS: [&str; 7] = ["byr", "eyr", "iyr", "ecl", "hcl", "hgt", "pid"];
+		const ECL_VALS: [&str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+		lazy_static! {
+			// This unwrap() will not cause error.
+			static ref HGT_REGEX: Regex = Regex::new(r"^(\w+)(\d+)$").unwrap();
+			static ref HCL_REGEX: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+			static ref PID_REGEX: Regex = Regex::new(r"^\d{9}$").unwrap();
+		}
+
 		let mut invalids = Vec::new();
-		for key in PASSPORT_REQ_KEYS.iter() {
-			if self.fields.get(*key).is_none() {
-				invalids.push(PassportInvalid::from(*key));
+
+		for &key in PASSPORT_REQ_KEYS.iter() {
+			match self.fields.get(key) {
+				Some(&PassportField::Num(val)) if key == "byr" && !(val >= 1920 && val <= 2002) => {
+					invalids.push(PassportInvalid::out_of_range(key, val));
+				},
+				Some(&PassportField::Num(val)) if key == "iyr" && !(val >= 2010 && val <= 2020) => {
+					invalids.push(PassportInvalid::out_of_range(key, val));
+				},
+				Some(&PassportField::Num(val)) if key == "eyr" && !(val >= 2020 && val <= 2030) => {
+					invalids.push(PassportInvalid::out_of_range(key, val));
+				},
+				Some(&PassportField::Str(ref val)) if key == "hgt" => {
+					// if let Some(captures) = HGT_REGEX.captures(val) {
+					// 	let omeasure = captures.get(1);
+					// 	let ounit = captures.get(2);
+					// 	if let Some(unit) = match ounit {
+					// 		if unit != "cm" && unit != "in" {
+					// 			invalids.push(PassportInvalid::InvalidFormat(key, val));
+					// 		}
+					// 		match measure.parse::<u32>() {
+					// 			Ok(hgt_val) => {
+					// 				if (unit == "cm" && !(hgt_val >= 150 && hgt_val <= 193)) ||
+					// 					(unit == "in" && !(hgt_val >= 59 && hgt_val <= 76)) {
+					// 					invalids.push(PassportInvalid::OutOfRange(key, val));
+					// 				}
+					// 			},
+					// 			Err(_) => {
+					// 				invalids.push(PassportInvalid::InvalidFormat(key, val));
+					// 			}
+					// 		}
+					// 	} else {
+					// 		invalids.push(PassportInvalid::InvalidFormat(key, val));
+					// 	}
+					// } else {
+					// 	invalids.push(PassportInvalid::InvalidFormat(key, val));
+					// }
+				},
+				Some(&PassportField::Str(ref val)) if key == "hcl" && !HCL_REGEX.is_match(val) => {
+					invalids.push(PassportInvalid::invalid_format(key, val));
+				},
+				Some(&PassportField::Str(ref val)) if key == "ecl" && !ECL_VALS.contains(&val.as_ref()) => {
+					invalids.push(PassportInvalid::invalid_format(key, val));
+				},
+				Some(&PassportField::Str(ref val)) if key == "pid" && !PID_REGEX.is_match(val) => {
+					invalids.push(PassportInvalid::invalid_format(key, val));
+				},
+				Some(_) => {}, // this line is needed so those pass the above will become accepted here
+				None => { invalids.push(PassportInvalid::from(key)) },
 			}
 		}
 
