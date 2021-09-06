8e426902b4878073bef297b90de7efd56fe27fe7
Jimmy Chu <jimmychu0807@gmail.com>
2021-09-06 10:31:11 +0800

    Completed Day 07 - Part 1

--- 07-handy-haversacks/src/bin/main.rs
+++ 07-handy-haversacks/src/bin/main.rs
@@ -1,5 +1,7 @@
+#![allow(dead_code)]
+
 use shared::{read_file_and_group, vec_string_to_str};
-use handy_haversacks::{build_rules};
+use handy_haversacks::{Haversacks};
 
 const TEST_PATH: &str = "07-handy-haversacks/data/test.dat";
 
@@ -7,15 +9,13 @@ const INPUT_PATH: &str = "07-handy-haversacks/data/input.dat";
 
 fn main() -> Result<(), &'static str> {
 
-	let groups = read_file_and_group(TEST_PATH)?;
+	let groups = read_file_and_group(INPUT_PATH)?;
 	let groups: Vec<_> = groups.into_iter().flatten().collect();
 
-	println!("{:?}", groups);
+	let haversacks = Haversacks::setup(vec_string_to_str(&groups));
+	let num = haversacks.find_contained_qty(&"shiny gold");
 
-	let haversacks = build_rules(vec_string_to_str(&groups));
-	let num = haversacks.find_outer_qty(&"shiny gold");
-
-	println!("{:?}", num);
+	println!("{} bag colors can eventually contain at least one shiny gold bag.", num);
 
 	Ok(())
 }
--- 07-handy-haversacks/src/lib.rs
+++ 07-handy-haversacks/src/lib.rs
@@ -1,17 +1,83 @@
 use std::collections::BTreeMap;
+use regex::Regex;
+use lazy_static::lazy_static;
 
+lazy_static! {
+	static ref IGNORE_RES: [regex::Regex; 1] = [
+		Regex::new(r"contain no other bags\.$").unwrap(),
+	];
+
+	static ref KEY_RE: Regex = Regex::new(r"^\s*(\w+\s+\w+)\s+bags\s*$")
+		.unwrap();
+
+	static ref NUM_BAG_RE: Regex = Regex::new(r"^\s*(\d+)\s+(\w+\s+\w+)\s+bags?\.?$")
+		.unwrap();
+}
+
+#[derive(Debug)]
 pub struct Haversacks {
-	rules: BTreeMap<String, Vec<(String, u32)>>
+	bag_rules: BTreeMap<String, Vec<(u32, String)>>
 }
 
 impl Haversacks {
-	pub fn find_outer_qty(&self, bag: &str) -> u32 {
-		1
+	pub fn setup(rule_arr: Vec<&str>) -> Self {
+		let mut bag_rules = BTreeMap::<String, Vec<(u32, String)>>::new();
+
+		'rules: for rule_str in rule_arr.iter() {
+			for ignore_re in IGNORE_RES.iter() {
+				if ignore_re.captures(rule_str).is_some() {
+					continue 'rules;
+				}
+			}
+
+			let mut main_splitted = rule_str.split("contain");
+			let key_str = main_splitted.nth(0).unwrap(); // `nth()` will consume the element
+			let bags_str = main_splitted.nth(0).unwrap();
+
+			let key_cap = KEY_RE.captures(key_str).expect("should returned key");
+			let key = key_cap.get(1).unwrap().as_str();
+
+			let rule_vec: Vec<_> = bags_str.split(",").map(|bag_str| {
+				let num_bag_cap = NUM_BAG_RE.captures(bag_str)
+					.expect("should capture num and bag string");
+
+				let num: u32 = num_bag_cap.get(1).expect("should capture a number")
+					.as_str().parse::<u32>().expect("should be convertible to a u32 integer");
+
+				let bag = num_bag_cap.get(2).expect("should capture a bag style").as_str();
+
+				(num, bag.to_string())
+			}).collect();
+
+			bag_rules.insert(key.to_string(), rule_vec);
+		}
+
+		Haversacks { bag_rules }
+	}
+
+	pub fn find_contained_qty(&self, target: &str) -> u32 {
+		self.bag_rules.keys()
+			.filter(|bag_key| bag_key.as_str() != target)
+			.map(|bag_key| self.contain_eventually(bag_key, target))
+			.fold(0, |acc, val| {
+				match val {
+					true => acc + 1,
+					false => acc
+				}
+			})
+	}
+
+	fn contain_eventually(&self, bag_key: &str, target: &str) -> bool {
+		if bag_key == target {
+			true
+		}
+		else if let Some(rule_vec) = self.bag_rules.get(bag_key) {
+			rule_vec.iter()
+				.map(|(_, bag)| self.contain_eventually(bag, target))
+				.fold(false, |acc, result| acc || result)
+		} else {
+			false
+		}
 	}
 }
 
-pub fn build_rules(rule_arr: Vec<&str>) -> Haversacks {
-	Haversacks {
-		rules: BTreeMap::new()
-	}
-}
