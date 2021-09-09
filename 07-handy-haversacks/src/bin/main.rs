d4ee46e8d5475eb5796cf7b60b6d216aae3f60f1
Jimmy Chu <jimmychu0807@gmail.com>
2021-09-09 10:01:21 +0800

    Completed Day 07

--- 07-handy-haversacks/src/bin/main.rs
+++ 07-handy-haversacks/src/bin/main.rs
@@ -1,21 +1,25 @@
-#![allow(dead_code)]
-
 use shared::{read_file_and_group, vec_string_to_str};
 use handy_haversacks::{Haversacks};
 
-const TEST_PATH: &str = "07-handy-haversacks/data/test.dat";
+#[allow(dead_code)]
+const TEST1_PATH: &str = "07-handy-haversacks/data/test1.dat";
 
+#[allow(dead_code)]
+const TEST2_PATH: &str = "07-handy-haversacks/data/test2.dat";
+
+#[allow(dead_code)]
 const INPUT_PATH: &str = "07-handy-haversacks/data/input.dat";
 
 fn main() -> Result<(), &'static str> {
-
 	let groups = read_file_and_group(INPUT_PATH)?;
 	let groups: Vec<_> = groups.into_iter().flatten().collect();
 
 	let haversacks = Haversacks::setup(vec_string_to_str(&groups));
-	let num = haversacks.find_contained_qty(&"shiny gold");
+	let qty = haversacks.contained_qty(&"shiny gold");
+	println!("{} bag colors can eventually contain at least one shiny gold bag.", qty);
 
-	println!("{} bag colors can eventually contain at least one shiny gold bag.", num);
+	let sum = haversacks.contained_sum(&"shiny gold");
+	println!("{} bags are required inside your single shiny gold bag.", sum);
 
 	Ok(())
 }
--- 07-handy-haversacks/src/lib.rs
+++ 07-handy-haversacks/src/lib.rs
@@ -16,12 +16,12 @@ lazy_static! {
 
 #[derive(Debug)]
 pub struct Haversacks {
-	bag_rules: BTreeMap<String, Vec<(u32, String)>>
+	bag_rules: BTreeMap<String, Vec<(u64, String)>>
 }
 
 impl Haversacks {
 	pub fn setup(rule_arr: Vec<&str>) -> Self {
-		let mut bag_rules = BTreeMap::<String, Vec<(u32, String)>>::new();
+		let mut bag_rules = BTreeMap::<String, Vec<(u64, String)>>::new();
 
 		'rules: for rule_str in rule_arr.iter() {
 			for ignore_re in IGNORE_RES.iter() {
@@ -41,8 +41,8 @@ impl Haversacks {
 				let num_bag_cap = NUM_BAG_RE.captures(bag_str)
 					.expect("should capture num and bag string");
 
-				let num: u32 = num_bag_cap.get(1).expect("should capture a number")
-					.as_str().parse::<u32>().expect("should be convertible to a u32 integer");
+				let num: u64 = num_bag_cap.get(1).expect("should capture a number")
+					.as_str().parse::<u64>().expect("should be convertible to a u64 integer");
 
 				let bag = num_bag_cap.get(2).expect("should capture a bag style").as_str();
 
@@ -55,10 +55,10 @@ impl Haversacks {
 		Haversacks { bag_rules }
 	}
 
-	pub fn find_contained_qty(&self, target: &str) -> u32 {
+	pub fn contained_qty(&self, target: &str) -> u64 {
 		self.bag_rules.keys()
 			.filter(|bag_key| bag_key.as_str() != target)
-			.map(|bag_key| self.contain_eventually(bag_key, target))
+			.map(|bag_key| self.contained_eventually(bag_key, target))
 			.fold(0, |acc, val| {
 				match val {
 					true => acc + 1,
@@ -67,17 +67,25 @@ impl Haversacks {
 			})
 	}
 
-	fn contain_eventually(&self, bag_key: &str, target: &str) -> bool {
+	fn contained_eventually(&self, bag_key: &str, target: &str) -> bool {
 		if bag_key == target {
 			true
 		}
 		else if let Some(rule_vec) = self.bag_rules.get(bag_key) {
 			rule_vec.iter()
-				.map(|(_, bag)| self.contain_eventually(bag, target))
+				.map(|(_, bag)| self.contained_eventually(bag, target))
 				.fold(false, |acc, result| acc || result)
 		} else {
 			false
 		}
 	}
-}
 
+	pub fn contained_sum(&self, target: &str) -> u64 {
+		if let Some(rule_vec) = self.bag_rules.get(target) {
+			rule_vec.iter()
+				.fold(0, |acc, rule| acc + rule.0 + rule.0 * self.contained_sum(&rule.1))
+		} else {
+			0
+		}
+	}
+}
