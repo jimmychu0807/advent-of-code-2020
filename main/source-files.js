b14438d3047e0fda8728756a7faefc600a4c218a
jimmychu0807 <gitlab-ci@hkwtf.com>
2021-10-13 08:45:25 +0000

    Updated docs for jc/ci-rustdocs

--- main/source-files.js
+++ main/source-files.js
@@ -1,6 +1,7 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["binary_boarding"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["custom_customs"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["encoding_error"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["handheld_halting"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["handy_haversacks"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["passport_processing"] = {"name":"","files":["lib.rs"]};
