7fb232b4a2233194e6b7b4f3a79fe948eafaacb2
Jimmy Chu <jimmychu0807@gmail.com>
2022-01-06 13:15:56 +0800

    Updated

--- .maintain/rustdocs-release.sh
+++ .maintain/rustdocs-release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
+set -ex
 
 # 1. checkout the certain branch / tag
 # 2. Run `cargo build to build the docs`
@@ -11,42 +11,81 @@ set -x
 
 # Example:
 #   rustdocs-release.sh --help
-#   rustdocs-release.sh deploy monthly-2021-12
-#   rustdocs-release.sh deploy --latest monthly-2021-12
-#   rustdocs-release.sh remove monthly-2021-12
+#   rustdocs-release.sh deploy monthly-2021-10
+#   rustdocs-release.sh deploy --latest monthly-2021-10
+#   rustdocs-release.sh remove monthly-2021-10
 
 # The git repo http URL
 # REMOTE_REPO="https://github.com/paritytech/substrate.git"
 REMOTE_REPO="https://github.com/jimmychu0807/advent-of-code-2020.git"
 
 # tmp location that the built doc is copied to. Better be an absolute path
-TMP_DOC_PREFIX="/tmp"
-DOC_INDEX_PAGE="sc_service/index.html"
+TMP_PREFIX="/tmp"
+DOC_INDEX_PAGE="binary_boarding/index.html"
 CARGO_NIGHTLY=true
+GIT_REMOTE="gh-me"
 
 # 1. checkout the certain branch / tag
 # Assuming we check out from the Substrate repo
 SCRIPT=$(realpath $0)
 SCRIPT_PATH=$(dirname $SCRIPT)
-PROJECT_DIR=$(dirname ${SCRIPT_PATH})
+PROJECT_PATH=$(dirname ${SCRIPT_PATH})
+PROJECT_NAME=$(basename "$PROJECT_PATH")
 
 SUBCMD=$1
 BUILD_RUSTDOC_REF=$2
+LATEST=true
+
+# Check there is no local changes before proceeding
+[[ -n $(git status --porcelain) ]] && echo "Local changes exist, please either discard or commit them as this command will change the current checkout branch." && exit 0
+
+CURRENT_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TMP_PROJECT_PATH="${TMP_PREFIX}/${PROJECT_NAME}"
+DOC_PATH="${TMP_PROJECT_PATH}/${BUILD_RUSTDOC_REF}"
+
+rm -rf "${TMP_PROJECT_PATH}" && mkdir "${TMP_PROJECT_PATH}"
+
+# Copy .gitignore file to tmp
+[[ -e "${PROJECT_PATH}/.gitignore" ]] && cp "${PROJECT_PATH}/.gitignore" "${TMP_PROJECT_PATH}"
 
 git fetch --all
 git checkout -f $BUILD_RUSTDOC_REF || { echo "Checkout ${BUILD_RUSTDOC_REF} error." && exit 0; };
 
+# Build the docs
 time cargo $($CARGO_NIGHTLY && echo "+nightly") doc --no-deps --workspace --all-features --verbose || \
   { echo "Generate $BUILD_RUSTDOC_REF rustdocs failed" && exit 0; }
-
 rm -f target/doc/.lock
 
-TMP_DOC_PATH="${TMP_DOC_PREFIX}/${PROJECT_DIR}"
-rm -rf "${TMP_DOC_PATH}"
-mkdir "${TMP_DOC_PATH}"
-mv target/doc "${TMP_DOC_PATH}/${BUILD_RUSTDOC_REF}"
+# Moving the built doc to the tmp location
+mv target/doc "${DOC_PATH}"
 
-[[ -z "${DOC_INDEX_PAGE}" ]] && \
-  echo "<meta http-equiv=refresh content=0;url=${DOC_INDEX_PAGE}>" > "${TMP_DOC_PATH}/${BUILD_RUSTDOC_REF}/index.html"
+[[ -n "${DOC_INDEX_PAGE}" ]] && \
+  echo "<meta http-equiv=refresh content=0;url=${DOC_INDEX_PAGE}>" > "${DOC_PATH}/index.html"
 
-popd
+# git checkout `gh-pages` branch
+git fetch "${GIT_REMOTE}" gh-pages
+yarn global add ejs
+# TODO
+# ejs ${INDEX_TPL} -i "{\"deploy_refs\":\"${RUSTDOCS_DEPLOY_REFS}\",\"repo_name\":\"${CI_PROJECT_NAME}\",\"latest\":\"${LATEST}\"}" > /tmp/index.html
+echo "My index page" > "${TMP_PROJECT_PATH}/index.html"
+
+
+git checkout gh-pages
+# Move the index page & built back
+cp -f "${TMP_PROJECT_PATH}/index.html" .
+[[ -e "${TMP_PROJECT_PATH}/.gitignore" ]] && cp -f "${TMP_PROJECT_PATH}/.gitignore" .
+# Ensure the destination dir doesn't exist under current path.
+rm -rf "${BUILD_RUSTDOC_REF}"
+mv -f "${DOC_PATH}" "${BUILD_RUSTDOC_REF}"
+# Add the symlink
+$LATEST && rm -rf latest && ln -sf "${BUILD_RUSTDOC_REF}" latest
+# Upload files
+git add --all
+git commit -m "___Updated docs for ${BUILD_RUSTDOC_REF}___" || echo "___Nothing to commit___"
+# git push "${GIT_REMOTE}" gh-pages --force
+
+# Remove the tmp asset created
+rm -rf "${TMP_PROJECT_PATH}"
+
+# Resume back previous checkout branch.
+git checkout -f "$CURRENT_GIT_BRANCH"
