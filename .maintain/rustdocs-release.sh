cada1afb75a1bf587abdbba14f75c0c6541ab36e
Jimmy Chu <jimmychu0807@gmail.com>
2022-01-07 11:17:38 +0800

    updated

--- .maintain/rustdocs-release.sh
+++ .maintain/rustdocs-release.sh
@@ -1,29 +1,82 @@
 #!/usr/bin/env bash
-set -x
+# set -x
 
-# 1. checkout the certain branch / tag
-# 2. Run `cargo build to build the docs`
-# 3. copy the doc folder over to `/tmp`
-# 4. Now, you need to switch your substrate folder branch to gh-page
-#    - copy the doc folder to this folder,
-#    - if `latest` flag is added, update the `index.html`
-#    - git force push back to the substrate git repo
-
-# Example:
-#   rustdocs-release.sh --help
+# Examples:
+#   rustdocs-release.sh -h
 #   rustdocs-release.sh deploy monthly-2021-10
-#   rustdocs-release.sh deploy --latest monthly-2021-10
+#   rustdocs-release.sh deploy -l monthly-2021-10
 #   rustdocs-release.sh remove monthly-2021-10
+
+# Script setting
 
 # The git repo http URL
 # REMOTE_REPO="https://github.com/paritytech/substrate.git"
 REMOTE_REPO="https://github.com/jimmychu0807/advent-of-code-2020.git"
-
-# tmp location that the built doc is copied to. Better be an absolute path
-TMP_PREFIX="/tmp"
+TMP_PREFIX="/tmp"                             # tmp location that the built doc is copied to.
 DOC_INDEX_PAGE="binary_boarding/index.html"
 CARGO_NIGHTLY=true
 GIT_REMOTE="gh-me"
+
+declare -A HELP_TXT
+HELP_TXT["deploy"]=$(cat <<-EOH
+Build and deploy the rustdocs of the specified branch/tag to \`gh-pages\` branch.
+
+  usage:      $0 deploy [-l] <git_branch_ref>
+  example:    $0 deploy -l monthly-2021-10
+
+  options:
+    -l        The \`latest\` path will be sym'linked to this version of rustdocs
+EOH
+)
+
+HELP_TXT["remove"]=$(cat <<-EOH
+Remove the rustdocs of the specified version from \`gh-pages\` branch.
+
+  usage:      $0 remove <git_branch_ref>
+  example:    $0 remove monthly-2021-10
+EOH
+)
+
+LATEST=false
+
+# Arguments handling
+SUBCMD=$1
+[[ $SUBCMD == "deploy" || $SUBCMD == "remove" ]] \
+  || { echo "Please specify a subcommand of \`deploy\` or \`remove\`" && exit 1 ; }
+shift
+
+# After removing the subcommand, there could only be 1 or 2 parameters afterward
+[[ $# -lt 1 || $# -gt 2 ]] && {
+  echo "${HELP_TXT[${SUBCMD}]}";
+  exit 1;
+}
+
+while getopts :lh opt; do
+  case $opt in
+    l)
+      LATEST=true
+      ;;
+    h)
+      echo "${HELP_TXT[${SUBCMD}]}"
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+BUILD_RUSTDOC_REF=$1
+
+[[ -z "$BUILD_RUSTDOC_REF" ]] && {
+  echo -e "git branch_ref is not specified.\n"
+  echo "${HELP_TXT[${SUBCMD}]}"
+  exit 1;
+}
+
+exit 0;
 
 # 1. checkout the certain branch / tag
 # Assuming we check out from the Substrate repo
@@ -32,21 +85,17 @@ SCRIPT_PATH=$(dirname $SCRIPT)
 PROJECT_PATH=$(dirname ${SCRIPT_PATH})
 PROJECT_NAME=$(basename "$PROJECT_PATH")
 
-SUBCMD=$1
-BUILD_RUSTDOC_REF=$2
-LATEST=true
-
 check_local_change() {
   # Check there is no local changes before proceeding
   [[ -n $(git status --porcelain) ]] \
     && echo "Local changes exist, please either discard or commit them as this command will change the current checkout branch." \
-    && exit 0
+    && exit 1
 }
 
 build_rustdocs() {
   # Build the docs
   time cargo $($CARGO_NIGHTLY && echo "+nightly") doc --no-deps --workspace --all-features --verbose \
-    || { echo "Generate $1 rustdocs failed" && exit 0; }
+    || { echo "Generate $1 rustdocs failed" && exit 1; }
   rm -f target/doc/.lock
 
   # Moving the built doc to the tmp location
@@ -75,7 +124,7 @@ main() {
   [[ -e "${PROJECT_PATH}/.gitignore" ]] && cp "${PROJECT_PATH}/.gitignore" "${TMP_PROJECT_PATH}"
 
   git fetch --all
-  git checkout -f ${BUILD_RUSTDOC_REF} || { echo "Checkout \`${BUILD_RUSTDOC_REF}\` error." && exit 0; };
+  git checkout -f ${BUILD_RUSTDOC_REF} || { echo "Checkout \`${BUILD_RUSTDOC_REF}\` error." && exit 1; }
   build_rustdocs "${BUILD_RUSTDOC_REF}" "${DOC_PATH}"
 
   # git checkout `gh-pages` branch
