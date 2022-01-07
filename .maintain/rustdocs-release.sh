3c008ab1adc902463fc13cf8605a11caedeb765a
Jimmy Chu <jimmychu0807@gmail.com>
2022-01-07 11:51:17 +0800

    arguments handling

--- .maintain/rustdocs-release.sh
+++ .maintain/rustdocs-release.sh
@@ -16,7 +16,9 @@ TMP_PREFIX="/tmp"                             # tmp location that the built doc 
 DOC_INDEX_PAGE="binary_boarding/index.html"
 CARGO_NIGHTLY=true
 GIT_REMOTE="gh-me"
+LATEST=false
 
+# Setting the help text
 declare -A HELP_TXT
 HELP_TXT["deploy"]=$(cat <<-EOH
 Build and deploy the rustdocs of the specified branch/tag to \`gh-pages\` branch.
@@ -25,7 +27,7 @@ Build and deploy the rustdocs of the specified branch/tag to \`gh-pages\` branch
   example:    $0 deploy -l monthly-2021-10
 
   options:
-    -l        The \`latest\` path will be sym'linked to this version of rustdocs
+    -l        The \`latest\` path will be sym'linked to this rustdocs version
 EOH
 )
 
@@ -37,53 +39,14 @@ Remove the rustdocs of the specified version from \`gh-pages\` branch.
 EOH
 )
 
-LATEST=false
-
-# Arguments handling
-SUBCMD=$1
-[[ $SUBCMD == "deploy" || $SUBCMD == "remove" ]] \
-  || { echo "Please specify a subcommand of \`deploy\` or \`remove\`" && exit 1 ; }
-shift
-
-# After removing the subcommand, there could only be 1 or 2 parameters afterward
-[[ $# -lt 1 || $# -gt 2 ]] && {
-  echo "${HELP_TXT[${SUBCMD}]}";
-  exit 1;
+set_and_check_rustdoc_ref() {
+  [[ -z "$1" ]] && {
+    echo -e "git branch_ref is not specified.\n"
+    echo "${HELP_TXT[$2]}"
+    exit 1
+  }
+  BUILD_RUSTDOC_REF=$1
 }
-
-while getopts :lh opt; do
-  case $opt in
-    l)
-      LATEST=true
-      ;;
-    h)
-      echo "${HELP_TXT[${SUBCMD}]}"
-      exit 0
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
-done
-shift $(($OPTIND - 1))
-
-BUILD_RUSTDOC_REF=$1
-
-[[ -z "$BUILD_RUSTDOC_REF" ]] && {
-  echo -e "git branch_ref is not specified.\n"
-  echo "${HELP_TXT[${SUBCMD}]}"
-  exit 1;
-}
-
-exit 0;
-
-# 1. checkout the certain branch / tag
-# Assuming we check out from the Substrate repo
-SCRIPT=$(realpath $0)
-SCRIPT_PATH=$(dirname $SCRIPT)
-PROJECT_PATH=$(dirname ${SCRIPT_PATH})
-PROJECT_NAME=$(basename "$PROJECT_PATH")
 
 check_local_change() {
   # Check there is no local changes before proceeding
@@ -104,13 +67,24 @@ build_rustdocs() {
     && echo "<meta http-equiv=refresh content=0;url=${DOC_INDEX_PAGE}>" > "${2}/index.html"
 }
 
-update_index_page() {
+upsert_index_page() {
   # Check if `index-tpl-crud` exists
   which index-tpl-crud &> /dev/null || yarn global add @jimmychu0807/index-tpl-crud
   index-tpl-crud upsert $($1 && echo "-l") ./index.html "$2"
 }
 
-main() {
+rm_index_page() {
+  which index-tpl-crud &> /dev/null || yarn global add @jimmychu0807/index-tpl-crud
+  index-tpl-crud rm ./index.html "$1"
+}
+
+git_add_commit_push() {
+  git add --all
+  git commit -m "$1" || echo "Nothing to commit"
+  git push "${GIT_REMOTE}" gh-pages --force
+}
+
+deploy_main() {
   check_local_change
 
   CURRENT_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -136,21 +110,95 @@ main() {
   rm -rf "${BUILD_RUSTDOC_REF}"
   mv -f "${DOC_PATH}" "${BUILD_RUSTDOC_REF}"
 
-  update_index_page $LATEST "${BUILD_RUSTDOC_REF}"
+  upsert_index_page $LATEST "${BUILD_RUSTDOC_REF}"
   # Add the latest symlink
   $LATEST && rm -rf latest && ln -sf "${BUILD_RUSTDOC_REF}" latest
 
-  # git commit and push
-  git add --all
-  git commit -m "___Updated docs for ${BUILD_RUSTDOC_REF}___" || echo "___Nothing to commit___"
-  git push "${GIT_REMOTE}" gh-pages --force
-
+  git_add_commit_push "___Deployed rustdocs of ${BUILD_RUSTDOC_REF}___"
   # Clean up
   # Remove the tmp asset created
   rm -rf "${TMP_PROJECT_PATH}"
+  # Resume back previous checkout branch.
+  git checkout -f "$CURRENT_GIT_BRANCH"
+}
+
+remove_main() {
+  check_local_change
+
+  CURRENT_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+  # git checkout `gh-pages` branch
+  git fetch "${GIT_REMOTE}" gh-pages
+  git checkout gh-pages
+
+  rm -rf "${BUILD_RUSTDOC_REF}"
+  rm_index_page "${BUILD_RUSTDOC_REF}"
+  # check if the destination of `latest` exists and rmove if not.
+  [[ -e "latest" ]] || rm latest
+
+  git_add_commit_push "___Removed rustdocs of ${BUILD_RUSTDOC_REF}___"
 
   # Resume back previous checkout branch.
   git checkout -f "$CURRENT_GIT_BRANCH"
 }
 
-main
+# ---- The script execution entry point starts here ----
+
+# Arguments handling
+SUBCMD=$1
+[[ $SUBCMD == "deploy" || $SUBCMD == "remove" ]] \
+  || { echo "Please specify a subcommand of \`deploy\` or \`remove\`" && exit 1 ;}
+shift
+
+# After removing the subcommand, there could only be 1 or 2 parameters afterward
+[[ $# -lt 1 || $# -gt 2 ]] && {
+  echo "${HELP_TXT[${SUBCMD}]}"
+  exit 1
+}
+
+# Parsing options and argument for `deploy` subcommand
+[[ $SUBCMD == "deploy" ]] && {
+  while getopts :lh opt; do
+    case $opt in
+      l)
+        LATEST=true
+        ;;
+      h)
+        echo "${HELP_TXT[$SUBCMD]}"
+        exit 0
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+    esac
+  done
+}
+# Parsing options and argument for `remove` subcommand
+[[ $SUBCMD == "remove" ]] && {
+  while getopts :h opt; do
+    case $opt in
+      h)
+        echo "${HELP_TXT[$SUBCMD]}"
+        exit 0
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+    esac
+  done
+}
+
+shift $(($OPTIND - 1))
+set_and_check_rustdoc_ref ${1:-''} $SUBCMD
+
+SCRIPT=$(realpath $0)
+SCRIPT_PATH=$(dirname $SCRIPT)
+PROJECT_PATH=$(dirname ${SCRIPT_PATH})
+PROJECT_NAME=$(basename "$PROJECT_PATH")
+
+pushd "${PROJECT_PATH}" &>/dev/null
+[[ $SUBCMD == "deploy" ]] && deploy_main
+[[ $SUBCMD == "remove" ]] && remove_main
+popd &>/dev/null
